0471e5ecc1ab6366d8a621865c568fe6df14e07a
Yane Vasquez <yanelisset@hotmail.com>
2018-09-24 23:57:05 -0300

    add exer date

--- basic/date/backup.sh
+++ basic/date/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Script para fazer backup
+#Script para fazer backup (do script para o dir)
 
 diretorio="backup"_$(date "+%d_%m_%y.%H.%S")
 echo "Copiando arquivos para: ${diretorio}"
