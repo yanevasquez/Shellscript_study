1f2e61bb1edec256a523a34a38efe4d522f3739a
Yane Vasquez <yanelisset@hotmail.com>
2018-09-25 00:06:34 -0300

    add exe2

--- basic/argumentos/exe2.sh
+++ basic/argumentos/exe2.sh
@@ -17,9 +17,7 @@
 #echo -e "${4} ${3}\t"
 #echo -e "${2} ${1}\t"
 
-#c) 
-
-#executar: ./exe2.sh a b c d e f g h i j
+#c) executar: ./exe2.sh a b c d e f g h i j
 
 echo "j:${10} i:$9 h:$8 g:$7 f:$6 e:$5 d:$4 c:$3 b:$2 a:$1"
 
