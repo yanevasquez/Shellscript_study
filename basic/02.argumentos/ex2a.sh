672fb0a96f39a8edc4841186409e6faa905700cf
Yane Vasquez <yanelisset@hotmail.com>
2018-10-05 16:07:48 -0300

    add ex2a,b,c

---
+++ basic/02.argumentos/ex2a.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#executar ./exe2.sh a b c d e f g h i j
+
+echo "${10} ${9} ${8} ${7} ${6} ${5} ${4} ${3} ${2} ${1}"
---
+++ basic/02.argumentos/ex2c.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#executar: ./exe2.sh a b c d e f g h i j
+
+echo "j:${10} i:$9 h:$8 g:$7 f:$6 e:$5 d:$4 c:$3 b:$2 a:$1"
+
+
