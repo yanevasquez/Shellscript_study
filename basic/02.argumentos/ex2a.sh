209c546e581c7d091d40e0bc9605279dd7077a02
Yane Vasquez <yanelisset@hotmail.com>
2019-03-18 14:42:30 -0300

    rm -r basic

--- basic/02.argumentos/ex2a.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#executar ./exe2.sh a b c d e f g h i j
-
-echo "${10} ${9} ${8} ${7} ${6} ${5} ${4} ${3} ${2} ${1}"
--- basic/03.data_subshell/ex2a.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-read -p "Informe uma data do formato (YYYY-mm-dd):" data
-
-date --date=${data} "+%A"
-
-
-
